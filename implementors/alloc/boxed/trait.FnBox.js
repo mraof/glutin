c12830869864c97d29bcd161122d2bbfe9dbe778
Travis CI <noreply@travis-ci.com>
2016-06-27 07:42:33 +0000

    Update documentation

---
+++ implementors/alloc/boxed/trait.FnBox.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["shared_library"] = [];implementors["serde"] = [];implementors["gleam"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
